9168dd728bb78f7bc050e427e64a966d5aa71424
Samantha Campbell <samantha.l.campbell1985@gmail.com>
2017-06-18 17:48:24 +0100

    adoptions.sql populated

---
+++ db/adoptions.sql
@@ -0,0 +1,25 @@
+DROP TABLE adoptions
+DROP TABLE animals;
+DROP TABLE owners;
+
+CREATE TABLE owners
+(
+id SERIAL4 PRIMARY KEY,
+name VARCHAR(255)
+);
+
+CREATE TABLE animals
+(
+id SERIAL4 PRIMARY KEY,
+name VARCHAR(255),
+admission_date DATE,
+adoptable VARCHAR(255),
+type VARCHAR(255));
+
+CREATE TABLE
+(
+id SERIAL4 PRIMARY KEY,
+date_of_adoption DATE,
+owner_id INT4 references owners(id),
+animal_id INT4 references animals(id)
+);
