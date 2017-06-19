cd8f16dd2df408c26cb075706574d4504c750628
Samantha Campbell <samantha.l.campbell1985@gmail.com>
2017-06-19 09:37:39 +0100

    adoption_controller populated

---
+++ controllers/adoption_controller.rb
@@ -0,0 +1,43 @@
+require('pry-byebug')
+require_relative('../models/adoption.rb')
+require_relative('../models/owner.rb')
+require_relative('../models/adoption.rb')
+
+get '/adoptions' do
+  @adoptions = Adoption.all()
+  erb (:"adoptions/index")
+end
+
+get '/adoptions/new' do
+  @adoptions = Adoption.all
+  erb (:"adoptions/new")
+end
+
+post '/adoptions' do
+  adoption = Adoption.new(params)
+  adoption.save
+  redirect to("/adoptions")
+end
+
+post '/adoptions/:id/delete' do
+  Adoption.destroy(params[:id])
+  redirect to("/adoptions")
+end
+
+
+post '/adoptions/new' do
+  @adoption = Adoption.new( params )
+  @adoption.save()
+  erb( :create)
+end
+
+# get '/owners/:id/edit' do
+#   @owner = Owner.find(params["id"])
+#   @adoption = Adoption.all()
+#   erb(:edit)
+# end
+
+# get '/owners/:id' do 
+#   @owner = Owner.find( params[:id])
+#   erb( :show)
+#   end
