5f9c4bf6f876eba753a77efdd702b4b0818c9f11
Samantha Campbell <samantha.l.campbell1985@gmail.com>
2017-06-19 09:24:42 +0100

    animals_controller populated

---
+++ controllers/animals_controller.rb
@@ -0,0 +1,25 @@
+require('pry-byebug')
+require_relative('../models/animal.rb')
+require_relative('../models/owner.rb')
+require_relative('../models/adoption.rb')
+
+get '/animals' do
+  @animals = Animal.all()
+  erb (:"animals/index")
+end
+
+get '/animals/new' do
+  @animals = Animal.all
+  erb (:"animals/new")
+end
+
+post '/animals' do
+  animal = Animal.new(params)
+  owner.save
+  redirect to("/animals")
+end
+
+post '/animals/:id/delete' do
+  Owner.destroy(params[:id])
+  redirect to("/animals")
+end
